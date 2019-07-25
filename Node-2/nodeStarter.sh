e9db171398b0d4d11cff564a88eb187577148bf8
Erick <eum602@gmail.com>
2019-07-25 13:25:25 -0500

    adjusted gasLimit to 10M -> 140KB data tx allowed

--- Node-2/nodeStarter.sh
+++ Node-2/nodeStarter.sh
@@ -1,4 +1,8 @@
-pantheon --data-path=data --genesis-file=../genesis.json --bootnodes="enode://7cecd0cf7522b57709c2666f2f147590588483cc6538f1562350d1dfe2068f0dd4f60b55fbe569e305c05cb52a7c8cdc5262fe7a662343bfdaf912cf3a37b8e4@127.0.0.1:30303" --p2p-port=30304 --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-whitelist="*" --rpc-http-cors-origins="all" --rpc-http-port=8546
+. ../env.sh
+pantheon --data-path=data --genesis-file=../genesis.json \
+--bootnodes=$PANTHEON_BOOTNODE \
+--p2p-port=30304 --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-whitelist="*" \
+--rpc-http-cors-origins="all" --rpc-http-port=8546
 #Bootnode in the example is the node one which is the bootnode 
 #p2p-port is for peer discovery
 #All hosts can access the HTTP JSON-RPC API using the --host-whitelist option
--- Node-4/nodeStarter.sh
+++ Node-4/nodeStarter.sh
@@ -1,1 +1,5 @@
-pantheon --data-path=data --genesis-file=../genesis.json --bootnodes="enode://7cecd0cf7522b57709c2666f2f147590588483cc6538f1562350d1dfe2068f0dd4f60b55fbe569e305c05cb52a7c8cdc5262fe7a662343bfdaf912cf3a37b8e4@127.0.0.1:30303" --p2p-port=30306 --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-whitelist="*" --rpc-http-cors-origins="all" --rpc-http-port=8548
+. ../env.sh
+pantheon --data-path=data --genesis-file=../genesis.json \
+--bootnodes=$PANTHEON_BOOTNODE \
+--p2p-port=30306 --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-whitelist="*" \
+--rpc-http-cors-origins="all" --rpc-http-port=8548
