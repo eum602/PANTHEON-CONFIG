25e418582bf46b5f8c670cd19336e59918ec86cc
Erick <eum602@gmail.com>
2019-07-24 21:30:43 -0500

    added scripts to start nodes and script to test the network

---
+++ Node-1/nodeStarter.sh
@@ -0,0 +1,5 @@
+pantheon --data-path=data --genesis-file=../genesis.json --rpc-http-enabled \
+--rpc-http-api=ETH,NET,IBFT --host-whitelist="*" \
+--rpc-http-cors-origins="all"
+#All hosts can access the HTTP JSON-RPC API using the --host-whitelist option
+#do not forget copying generated genesis.json from netwokFiles folder to the root directory
