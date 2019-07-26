747c24da55a4be9882bb913e1861fbf707f11b6e
Erick <eum602@gmail.com>
2019-07-26 18:06:31 -0500

    tested new validator (node-5) by including this to vote to delete non working node-3

--- ibft_method_removeValidatorVote.sh
+++ ibft_method_removeValidatorVote.sh
@@ -8,13 +8,13 @@ echo "****Discard Validator Node****"
 #Method is similar to add a validator but the second element in the parameter
 #array is FALSE
 curl -X POST \
---data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["9e40ad783707a41d9e08b820d3a457d99be39e58",false], "id":1}' \
-http://127.0.0.1:8545 #removing from node-1
-
-curl -X POST \
---data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["9e40ad783707a41d9e08b820d3a457d99be39e58",false], "id":1}' \
+--data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["aa8984200bc8f1e38593038d80e0dbae5d904f30",false], "id":1}' \
 http://127.0.0.1:8546 #removing from node-2
 
 curl -X POST \
---data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["9e40ad783707a41d9e08b820d3a457d99be39e58",false], "id":1}' \
-http://127.0.0.1:8547 #removing from node-3
+--data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["aa8984200bc8f1e38593038d80e0dbae5d904f30",false], "id":1}' \
+http://127.0.0.1:8548 #removing from node-4
+
+curl -X POST \
+--data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["aa8984200bc8f1e38593038d80e0dbae5d904f30",false], "id":1}' \
+http://127.0.0.1:8549 #removing from new node-5!!!
