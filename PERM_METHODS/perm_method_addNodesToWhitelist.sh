f1e01fc7734e60f902a97d0fd94b73750b318068
Erick <eum602@gmail.com>
2019-08-07 15:53:52 -0500

    modified script to add nodes in the whitelist

--- PERM_METHODS/perm_method_addNodesToWhitelist.sh
+++ PERM_METHODS/perm_method_addNodesToWhitelist.sh
@@ -1,7 +1,7 @@
-enode=$1
+NODES=$1
 RPC_URL=$2
 echo "****Adding enode to Permissions Configuration file****"
 curl -X POST \
 --data "{\"jsonrpc\":\"2.0\",\"method\":\"perm_addNodesToWhitelist\",
-\"params\":[[\"$enode\"]], \"id\":1}" \
+\"params\":[$NODES], \"id\":1}" \
 $RPC_URL
