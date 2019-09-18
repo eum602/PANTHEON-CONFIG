d6b2356f4a8c0ba237bc792112d67f2bbb3ce946
Erick <eum602@gmail.com>
2019-09-18 11:32:49 -0500

    mdified nodeStarter of simple private network to refence to a local genesis.json

--- private-network/Node-2/nodeStarter.sh
+++ private-network/Node-2/nodeStarter.sh
@@ -1,8 +1,9 @@
 . ../../env.sh
 pantheon --data-path=data --genesis-file=../../genesis.json \
 --bootnodes=$PANTHEON_BOOTNODE \
---p2p-port=30304 --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-whitelist="*" \
---rpc-http-cors-origins="all" --rpc-http-port=8546
+--p2p-port=30304 --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT,MINER --host-whitelist="*" \
+--rpc-http-cors-origins="all" --rpc-http-port=8546 \
+--miner-coinbase="0xf17f52151EbEF6C7334FAD080c5704D77216b732" --miner-enabled
 #--rpc-http-enabled allows for adding or removing validators
 #Bootnode in the example is the node one which is the bootnode 
 #p2p-port is for peer discovery
