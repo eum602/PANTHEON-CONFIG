3d00157166af247307c67840db90accca11336b1
Erick <eum602@gmail.com>
2019-11-05 01:05:15 -0500

    added configuration for permissioning with onchain; this only should be done progressively by restarting each node with the permissioning flags; only when the network is running, it can be achieved by starting whole network without the permissioning flags. At this point permissioning is showing drawbacks for example when starting whole network with permissioning flags(obviously the contracts must be previously deployed and permissioned nodes added thorugh the DAPP) the node fails because nodes only connect with Bootnodes but do not connect with other validators, that is why adding permissioning progressively is a must.  Another important detail is that bootnodes works on onchain if at least one node needs to find another permissioned node, after that boonode is rejected, to avoid that the bootnode must be added to the node ingress contract in order that that boot where always connected to the network and were always helping new nodes to fing another onchain permissioned nodes.  ALso it has been observed that progressive adding process sometimes fails, but repeating it again can work. If you do not have whole control of validators you must ensure that at least connected validators >=2/3(whole validators) are working and after that you can start the progressive adding process of new validators on onchain smart contracts

--- permissioned-network-onchain/Node-2/nodeStarter.sh
+++ permissioned-network-onchain/Node-2/nodeStarter.sh
@@ -2,12 +2,11 @@
 #All hosts can access the HTTP JSON-RPC API using the --host-whitelist option
 pantheon --data-path=data --genesis-file=../genesis.json \
 --p2p-port=30304 \
+--permissions-accounts-contract-enabled \
+--permissions-accounts-contract-address=0x0000000000000000000000000000000000008888 \
+--permissions-nodes-contract-enabled \
+--permissions-nodes-contract-address=0x0000000000000000000000000000000000009999 \
 --rpc-http-enabled --rpc-http-api=ADMIN,ETH,NET,PERM,IBFT --host-whitelist="*" \
 --rpc-http-port=8546 \
 --rpc-http-cors-origins="all" \
 --bootnodes=$PANTHEON_BOOTNODE
-
-# --permissions-accounts-contract-enabled \
-# --permissions-accounts-contract-address=0x0000000000000000000000000000000000008888 \
-# --permissions-nodes-contract-enabled \
-# --permissions-nodes-contract-address=0x0000000000000000000000000000000000009999 \
