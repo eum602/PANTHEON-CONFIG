73e4d4747f8e7e1aff799c832d7ec5820f866a2b
Erick <eum602@gmail.com>
2019-11-12 15:58:46 -0500

    explained how cors works  and the difference with the whitelist

--- private-network/Node-1/nodeStarter.sh
+++ private-network/Node-1/nodeStarter.sh
@@ -7,5 +7,6 @@ pantheon --data-path=data --genesis-file=../genesis.json --rpc-http-enabled \
 #do not forget copying generated genesis.json from netwokFiles folder to the root directory
 #in host whitelist you can spedify domains or ips. It has been tested with config.toml:
 #rpc-http-host="0.0.0.0" ==> it specifies the node is not only listening in loopback but also listening on other network interfaces!
-#host-whitelist=["35.240.205.158"] #tested!!
+#host-whitelist=["35.240.205.158"] #tested!!, in this case the request is made directly from another server!
+#rpc-http-cors-origin=["http://someone.com","http://anotherone.com"] #allows which domain URL can access the node using JSON-RPC, default is "none"; in this case the request is made from a web page ("putting the web url allows pages can make a call from a web browser").
 
