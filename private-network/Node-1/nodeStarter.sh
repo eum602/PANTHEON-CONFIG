0d19e0a0c08b5df493162ad1d077450d17f5f49e
Erick <eum602@gmail.com>
2019-11-12 15:08:22 -0500

    added specification how to use rpc-http-host and host-whitelist

--- private-network/Node-1/nodeStarter.sh
+++ private-network/Node-1/nodeStarter.sh
@@ -1,5 +1,11 @@
 pantheon --data-path=data --genesis-file=../genesis.json --rpc-http-enabled \
 --rpc-http-api=ETH,NET,IBFT --host-whitelist="*" \
---rpc-http-cors-origins="all"
+--rpc-http-cors-origins="all" --rpc-http-port=8545 \
+--graphql-http-cors-origins="all" --graphql-http-enabled --graphql-http-port=6175 \
+--min-gas-price=0
 #All hosts can access the HTTP JSON-RPC API using the --host-whitelist option
 #do not forget copying generated genesis.json from netwokFiles folder to the root directory
+#in host whitelist you can spedify domains or ips. It has been tested with config.toml:
+#rpc-http-host="0.0.0.0" ==> it specifies the node is not only listening in loopback but also listening on other network interfaces!
+#host-whitelist=["35.240.205.158"] #tested!!
+
