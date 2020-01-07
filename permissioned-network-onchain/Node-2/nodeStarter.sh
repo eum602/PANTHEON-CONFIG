f6bd3c19f2a495d6dabe4e96bf98e6b4b4682552
Erick <eum602@gmail.com>
2020-01-07 17:28:33 -0500

    added bootnodes for validators - and regulars, also specified enodes for all nodes

--- permissioned-network-onchain/Node-2/nodeStarter.sh
+++ permissioned-network-onchain/Node-2/nodeStarter.sh
@@ -2,11 +2,9 @@
 #All hosts can access the HTTP JSON-RPC API using the --host-whitelist option
 pantheon --data-path=data --genesis-file=../genesis.json \
 --p2p-port=30304 \
---permissions-accounts-contract-enabled \
---permissions-accounts-contract-address=0x8002B7ae0032e39ec64ECe493AB727A8060eAb67 \
 --permissions-nodes-contract-enabled \
---permissions-nodes-contract-address=0x218d5fe2E168656eBDE49e7a4A3C97E699D0be78 \
+--permissions-nodes-contract-address=0x9FBDa871d559710256a2502A2517b794B482Db40 \
 --rpc-http-enabled --rpc-http-api=ADMIN,ETH,NET,PERM,IBFT --host-whitelist="*" \
 --rpc-http-port=8546 \
 --rpc-http-cors-origins="all" \
---bootnodes=$PANTHEON_BOOTNODE
+--bootnodes=$PANTHEON_VALIDATOR_BOOTNODE,$PANTHEON_BOOTNODE
