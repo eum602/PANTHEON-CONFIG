8126807ef27cf0015740242efa093c01e380bb0e
Erick <eum602@gmail.com>
2020-01-07 17:30:45 -0500

    specified different contract for Bootnode-regular, also specified addresses obtained after deploying => node and account addresses are the ones that have to be specified on each node configuration file

--- permissioned-network-onchain/Bootnode-1/nodeStarter.sh
+++ permissioned-network-onchain/Bootnode-1/nodeStarter.sh
@@ -1,19 +1,9 @@
 . ../env.sh
-#All hosts can access the HTTP JSON-RPC API using the --host-whitelist option
-# pantheon --data-path=data --genesis-file=../genesis.json \
-# --p2p-port=30302 \
-# --permissions-accounts-contract-enabled \
-# --permissions-accounts-contract-address=0x0000000000000000000000000000000000008888 \
-# --permissions-nodes-contract-enabled \
-# --permissions-nodes-contract-address=0x0000000000000000000000000000000000009999 \
-# --rpc-http-enabled --rpc-http-api=ADMIN,ETH,NET,PERM,IBFT --host-whitelist="*" \
-# --rpc-http-port=8544 \
-# --rpc-http-cors-origins="all" \
-# --bootnodes=$PANTHEON_BOOTNODE
-
 pantheon --data-path=data --genesis-file=../genesis.json \
 --p2p-port=30302 \
+--permissions-nodes-contract-enabled \
+--permissions-nodes-contract-address=0xBd2c938B9F6Bfc1A66368D08CB44dC3EB2aE27bE \
 --rpc-http-enabled --rpc-http-api=ADMIN,ETH,NET,PERM,IBFT --host-whitelist="*" \
 --rpc-http-port=8544 \
 --rpc-http-cors-origins="all" \
---bootnodes=$PANTHEON_BOOTNODE
+#--logging=ALL
